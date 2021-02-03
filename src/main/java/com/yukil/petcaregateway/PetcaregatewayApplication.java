7534ee8bda1a7c4b1488d89a11ab95165ebb5f04
yukil <dbrdlf61@gmail.com>
2021-02-03 21:55:36 +0900

    버전충돌로 인해 oauth2 추가 시 application 실행 불가 spring boot 2.1.5로 downgrade하고 spring cloud gateway -> netflix zuul로 변경

--- src/main/java/com/yukil/petcaregateway/PetcaregatewayApplication.java
+++ src/main/java/com/yukil/petcaregateway/PetcaregatewayApplication.java
@@ -2,12 +2,15 @@ package com.yukil.petcaregateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class PetcaregatewayApplication {
-
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class PetcaregatewayApplication  {
     public static void main(String[] args) {
         SpringApplication.run(PetcaregatewayApplication.class, args);
     }
